f4ee04d1c713e22b7b5323f804704250b156c551
Daniil Oberlev <denioberlev@gmail.com>
2025-09-20 22:17:45 +0300

    feat(libs): add simple libs to project

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
 
 func main() {
-	fmt.Println("Hello, backend!")
+	logger, _ := zap.NewProduction()
+
+	logger.Info("Backend started")
+
+	r := gin.Default()
+
+	r.GET("/", func(ctx *gin.Context) {
+		logger.Info("Received request", zap.String("path", ctx.Request.UserAgent()))
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "yo!",
+		})
+	})
+
+	if err := r.Run(":8081"); err != nil {
+		logger.Fatal("Backend is down :(")
+	}
+
+	if err := logger.Sync(); err != nil {
+		logger.Warn("Failed to sync logger", zap.Error(err))
+	}
 }
